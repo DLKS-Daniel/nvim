17e0d3f96d0bf739e5b8097601881900674e8dfe
DLKS-Daniel <DLKS@ramboll.com>
2025-09-30 08:50:37 +0200

    Add homec config tweaks

--- init.lua
+++ init.lua
@@ -26,51 +26,65 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.laststatus = 3
+
 vim.pack.add({
-    { src = "https://github.com/vimwiki/vimwiki" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/tpope/vim-fugitive" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/folke/tokyonight.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/nvim-mini/mini.nvim",            version = "main" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/folke/tokyonight.nvim.git" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/vimwiki/vimwiki" },
+    { src = "https://github.com/tpope/vim-fugitive" },
+    { src = "https://github.com/tpope/vim-surround" },
 })
+
 vim.g.vimwiki_list = { {
     path = '~\\vimwiki',
     syntax = 'markdown',
     ext = '.md'
 } }
+local choose_all = function()
+    local mappings = require("mini.pick").get_picker_opts().mappings
+    vim.api.nvim_input(mappings.mark_all .. mappings.choose_marked)
+end
 
 require("mason").setup()
-require("mini.pick").setup()
-require("mini.pairs").setup()
-require("mini.surround").setup()
-require("tokyonight").setup({ opts = { transparent = true } })
 require("oil").setup({ view_options = { show_hidden = true } })
+require("tokyonight").setup({ opts = { transparent = true } })
+require("mini.pairs").setup()
+require('mini.pick').setup({ mappings = { choose_all = { char = '<C-q>', func = choose_all } } })
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "python", "lua", "bash", "json", "markdown" },
+    ensure_installed = { "python", "lua", "bash", "markdown", "markdown_inline" },
     highlight = { enable = true }
 })
-vim.cmd [[colorscheme tokyonight-night]]
-vim.cmd [[hi statusline guibg=NONE]]
-
-vim.keymap.set("t", "<Esc>", "<c-\\><c-n>")
-vim.keymap.set("n", "<Backspace>", ":nohl<CR>")
-vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>")
-vim.keymap.set("n", "<leader>g", "<cmd>Git<CR>")
+vim.cmd [[colorscheme tokyonight-moon]]
+vim.cmd [[set statusline+=\ \|%n\|]]
+vim.cmd [[ hi statusline guibg=NONE ]]
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>")
+vim.keymap.set("n", "<leader>g", "<cmd>Git<cr>")
 vim.keymap.set("n", "<leader>y", "<cmd>%y+<CR>")
-vim.keymap.set("n", "<leader>t", "<cmd>bot terminal<CR>i")
-vim.keymap.set("n", "<leader>f", "<cmd>Pick files<CR>")
-vim.keymap.set("n", "<leader>r", "<cmd>Pick grep_live<CR>")
-vim.keymap.set('n', '<leader>q', require("mini.bufremove").delete)
-vim.keymap.set("n", "<C-n>", "<cmd>bnext<cr>")
-vim.keymap.set("n", "<C-p>", "<cmd>bprev<cr>")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "gD", vim.lsp.buf.definition)
+vim.keymap.set("n", "<leader>t", "<cmd>bot terminal<cr>i")
+vim.keymap.set("n", "<Backspace>", ":nohl<cr>")
+vim.keymap.set("n", "<leader>f", "<cmd>Pick files<cr>")
+vim.keymap.set("n", "<leader>r", "<cmd>Pick grep_live<cr>")
 vim.keymap.set("n", "<leader>js", vim.lsp.buf.format)
+vim.keymap.set("n", "gD", vim.lsp.buf.definition)
+vim.keymap.set("n", "<leader>q", require("mini.bufremove").delete)
+vim.keymap.set("n", "<C-n>", "<cmd>bnext<cr>")
+vim.keymap.set("n", "<C-p>", "<cmd>bprevious<cr>")
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("t", "<Esc>", "<c-\\><c-n>")
+vim.keymap.set("n", "<leader>ww", "<cmd>edit ~/vimwiki/index.md<cr>")
 vim.keymap.set("n", "<leader>l",
     function() vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text }) end)
+vim.keymap.set("i", "<CR>", function()
+    if vim.fn.pumvisible() == 1 then
+        return "<C-e><CR>"
+    else
+        return "<CR>"
+    end
+end, { expr = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('lsp_comp', {}),
@@ -93,4 +107,5 @@ vim.opt.foldlevel = 99
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.lsp.config('*', { root_markers = { '.git' } })
-vim.lsp.enable({ "lua_ls", "basedpyright", "jsonls", "marksman", "superhtml" })
+vim.diagnostic.config({ virtual_lines = true })
+vim.lsp.enable({ "lua_ls", "basedpyright", "jsonls", "ruff", "marksman", "superhtml" })
