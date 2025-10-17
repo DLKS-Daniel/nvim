c339339ff3911a7f1723c23936e781d352410607
DLKS-Daniel <DLKS@ramboll.com>
2025-10-17 08:32:02 +0200

    Add new keymaps and bash specific sourcing

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
-vim.o.shell = [["C:\Program Files\Git\bin\bash.exe"]]
-vim.o.shellcmdflag = "-c"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
-vim.o.shellslash = true
-vim.g.shell_aliases = true
+if vim.fn.getenv("MINGW_PREFIX") ~= vim.NIL then
+    dofile("C:\\Users\\DLKS\\.config\\nvim\\init.lua")
+end
 
 vim.g.mapleader = " "
 vim.opt.signcolumn = "yes:1"
@@ -24,19 +21,18 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.pumheight = 10
 vim.opt.shiftwidth = 2
-vim.opt.laststatus = 3
+vim.opt.laststatus = 2
 
 vim.pack.add({
     { src = "https://github.com/nvim-mini/mini.nvim",            version = "main" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/folke/tokyonight.nvim.git" },
-    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/folke/tokyonight.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/tpope/vim-surround" },
     { src = "https://github.com/vimwiki/vimwiki" }
 })
-vim.g.vimwiki_list = { { path = "~/Documents/Vimwiki", syntax = "markdown", ext = ".md" } }
+vim.g.vimwiki_list = { { path = "~/Vimwiki", syntax = "markdown", ext = ".md" } }
 vim.g.vimwiki_global_ext = 0
 
 local choose_all = function()
@@ -44,7 +40,6 @@ local choose_all = function()
     vim.api.nvim_input(mappings.mark_all .. mappings.choose_marked)
 end
 
-require("mason").setup()
 require("mini.pairs").setup()
 require("mini.diff").setup()
 require("oil").setup({ view_options = { show_hidden = true } })
@@ -58,25 +53,31 @@ require("nvim-treesitter.configs").setup({
     highlight = { enable = true }
 })
 
-vim.cmd [[colorscheme tokyonight-moon]]
+vim.cmd [[colorscheme tokyonight-night]]
 require("mini.misc").setup_termbg_sync()
-vim.cmd [[ hi statusline guibg=NONE ]]
 
+local opts = { noremap = true, silent = true }
 vim.keymap.set({ "n", "v" }, "æ", ":")
-vim.keymap.set("n", "U", "<C-R>")
-vim.keymap.set("n", "<C-n>", "<cmd>bnext<cr>")
-vim.keymap.set("n", "<C-p>", "<cmd>bprevious<cr>")
-vim.keymap.set("n", "<leader>t", "q:")
-vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>")
-vim.keymap.set("n", "<leader>g", "<cmd>Git<cr>")
-vim.keymap.set("n", "<leader>f", "<cmd>Pick files<cr>")
-vim.keymap.set("n", "<leader>r", "<cmd>Pick grep_live<cr>")
-vim.keymap.set("n", "<leader>js", vim.lsp.buf.format)
+vim.keymap.set("n", "qæ", "q:", opts)
+vim.keymap.set("n", "U", "<C-R>", opts)
+vim.keymap.set("n", "<C-n>", "<cmd>bnext<cr>", opts)
+vim.keymap.set("n", "<C-p>", "<cmd>bprevious<cr>", opts)
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", opts)
+vim.keymap.set("n", "<leader>g", "<cmd>Git<cr>", opts)
+vim.keymap.set("n", "<leader>f", "<cmd>Pick files<cr>", opts)
+vim.keymap.set("n", "<leader>r", "<cmd>Pick grep_live<cr>", opts)
+vim.keymap.set("n", "<leader>ps", "<cmd>!prettier -w %<cr>", opts)
+vim.keymap.set("n", "<leader>js", vim.lsp.buf.format, opts)
+vim.keymap.set("n", "<leader>?", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "grd", vim.diagnostic.setqflist, opts)
 vim.keymap.set("n", "<leader>q", require("mini.bufremove").delete)
-vim.keymap.set("n", "<Backspace>", ":nohl<cr>")
-vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
-vim.keymap.set("t", "<Esc>", "<c-\\><c-n>")
+vim.keymap.set("n", "go", require("mini.diff").toggle_overlay)
+vim.keymap.set("n", "<Backspace>", ":nohl<cr>", opts)
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+vim.keymap.set("t", "<Esc>", "<c-\\><c-n>", opts)
 vim.keymap.set("i", "<CR>", function()
     if vim.fn.pumvisible() == 1 then
         return "<C-e><CR>"
@@ -106,5 +107,4 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.diagnostic.config({ virtual_text = true })
-vim.lsp.enable({ "lua_ls", "basedpyright", "ruff", "denols", "jsonls" })
+vim.lsp.enable({ "lua_ls", "basedpyright", "ruff", "jsonls" }) -- Installed with winget, uv, and npm
