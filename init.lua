3b0186c290dfa52c9fda2a48fe3ea70ae3954a00
DLKS-Daniel <DLKS@ramboll.com>
2025-11-06 08:43:07 +0100

    push to github

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Formatters and LSPs installed with winget, uv, and npm
 if vim.fn.getenv("MINGW_PREFIX") ~= vim.NIL then
     dofile("C:\\Users\\DLKS\\.config\\nvim\\init.lua")
 end
@@ -9,7 +10,12 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = "menu,menuone,noinsert"
 vim.opt.statusline:prepend("(%n) ")
 vim.opt.fillchars = { diff = "╱" }
+vim.opt.pumheight = 10
+vim.opt.shiftwidth = 2
+vim.opt.laststatus = 2
+vim.opt.tabstop = 2
 vim.opt.number = true
+vim.opt.autoread = true
 vim.opt.undofile = true
 vim.opt.smartcase = true
 vim.opt.cursorline = true
@@ -18,19 +24,17 @@ vim.opt.ignorecase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.pumheight = 10
-vim.opt.shiftwidth = 2
-vim.opt.laststatus = 2
 
 vim.pack.add({
-    { src = "https://github.com/nvim-mini/mini.nvim",            version = "main" },
+    { src = "https://github.com/nvim-mini/mini.nvim", version = "main" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/folke/tokyonight.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/tpope/vim-surround" },
-    { src = "https://github.com/vimwiki/vimwiki" }
+    { src = "https://github.com/stevearc/conform.nvim" },
+    { src = "https://github.com/iamcco/markdown-preview.nvim" },
+    { src = "https://github.com/vimwiki/vimwiki" }, -- Substitute with dir shortcut and 'gf'
 })
 vim.g.vimwiki_list = { { path = "~/Vimwiki", syntax = "markdown", ext = ".md" } }
 vim.g.vimwiki_global_ext = 0
@@ -42,18 +46,30 @@ end
 
 require("mini.pairs").setup()
 require("mini.diff").setup()
+require("mini.ai").setup()
+require("mini.icons").setup()
 require("oil").setup({ view_options = { show_hidden = true } })
 require("tokyonight").setup({ opts = { transparent = true } })
-require('mini.pick').setup({
-    mappings = { choose_all = { char = '<C-q>', func = choose_all }
-    }
+require("mini.pick").setup({
+    mappings = { choose_all = { char = "<C-q>", func = choose_all } },
 })
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "python", "lua", "bash", "markdown", "markdown_inline" },
-    highlight = { enable = true }
+    ensure_installed = { "python", "bash", "json" },
+    highlight = { enable = true },
+})
+require("conform").setup({
+    formatters = {
+        ["*"] = { async = true },
+    },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+        json = { "jq" },
+        markdown = { "prettier" },
+    },
 })
 
-vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd([[colorscheme tokyonight-night]])
 require("mini.misc").setup_termbg_sync()
 
 local opts = { noremap = true, silent = true }
@@ -66,10 +82,10 @@ vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", opts)
 vim.keymap.set("n", "<leader>g", "<cmd>Git<cr>", opts)
 vim.keymap.set("n", "<leader>f", "<cmd>Pick files<cr>", opts)
 vim.keymap.set("n", "<leader>r", "<cmd>Pick grep_live<cr>", opts)
-vim.keymap.set("n", "<leader>ps", "<cmd>!prettier -w %<cr>", opts)
-vim.keymap.set("n", "<leader>js", vim.lsp.buf.format, opts)
+vim.keymap.set("n", "<leader>js", require("conform").format, opts)
 vim.keymap.set("n", "<leader>?", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "grd", vim.diagnostic.setqflist, opts)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "<leader>q", require("mini.bufremove").delete)
 vim.keymap.set("n", "go", require("mini.diff").toggle_overlay)
 vim.keymap.set("n", "<Backspace>", ":nohl<cr>", opts)
@@ -86,12 +102,15 @@ vim.keymap.set("i", "<CR>", function()
     end
 end, { expr = true })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('lsp_comp', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp_comp", {}),
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/completion') then
-            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+        if client:supports_method("textDocument/completion") then
+            local chars = {}
+            for i = 32, 126 do
+                table.insert(chars, string.char(i))
+            end
             client.server_capabilities.completionProvider.triggerCharacters = chars
             vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
         end
@@ -99,12 +118,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("hl_yank", { clear = true }),
     callback = function()
-        vim.hl.on_yank()
-    end
+        (vim.hl or vim.highlight).on_yank()
+    end,
 })
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.lsp.enable({ "lua_ls", "basedpyright", "ruff", "jsonls" }) -- Installed with winget, uv, and npm
+vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.lsp.enable({ "lua_ls", "basedpyright", "ruff", "jsonls" })
